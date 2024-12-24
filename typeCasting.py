c452591a8b8b7c6fded777f33f9e628553757837
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2024-12-24 07:55:51 +0600

    till math function

---
+++ typeCasting.py
@@ -0,0 +1,14 @@
+#Type casting = convert the data tpe of a value to another data type
+
+x = 1 #int
+y = 2.3 #float
+z = '3' #str
+
+y = int(y)
+z = int(z)
+x = float(x)
+
+
+print(x) #1.0 
+print(y) #2
+print(z) #3 as int
---
+++ userInput.py
@@ -0,0 +1,8 @@
+name = input("What is your name? : ")
+age = int(input("What is your age: "))
+height = float(input("What is your height: "))
+age +=1
+
+print("hello " + name)
+print('You are ' + str(age) + " years old")
+print('you are ' + str(height) + 'cm tall')
